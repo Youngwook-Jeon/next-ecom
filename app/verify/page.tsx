ad4456caf8e17aada2c7d2632f3b2e9cb668cb8d
Youngwook-Jeon <ela87@naver.com>
2024-02-16 20:37:03 +0900

    Setup the Notification component and render results from verifying token

--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -2,6 +2,7 @@
 
 import { redirect, useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { toast } from "react-toastify";
 
 interface Props {
   searchParams: { token: string; userId: string };
@@ -21,13 +22,14 @@ export default function Verify(props: Props) {
       const apiRes = await res.json();
       const { error, message } = apiRes as { message: string; error: string };
       if (res.ok) {
-        console.log(message);
+        toast.success(message);
         router.replace("/");
       } else if (error) {
-        console.log(error);
+        toast.error(error);
       }
+      router.replace("/");
     });
-  });
+  }, []);
 
   return (
     <div className="text-3xl opacity-70 text-center p-5 animate-pulse">

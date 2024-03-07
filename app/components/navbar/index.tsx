5b580601ef96acebb9ee0748d655cd85a713ffab
Youngwook-Jeon <ela87@naver.com>
2024-03-08 00:01:39 +0900

    Get total count of items in a cart

--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -1,9 +1,43 @@
+import { auth } from "@/auth";
 import NavUI from "./NavUI";
+import CartModel from "@models/cartModel";
+import { Types } from "mongoose";
 
-export default function Navbar() {
+const getCartItemsCount = async () => {
+  try {
+    const session = await auth();
+    if (!session?.user) return 0;
+
+    const userId = session.user.id;
+
+    const cart = await CartModel.aggregate([
+      { $match: { userId: new Types.ObjectId(userId) } },
+      { $unwind: "$items" },
+      {
+        $group: {
+          _id: "$_id",
+          totalQuantity: { $sum: "$items.quantity" },
+        },
+      },
+    ]);
+
+    if (cart.length) {
+      return cart[0].totalQuantity;
+    }
+
+    return 0;
+  } catch (error) {
+    console.log("Error while fetching cart items.", error);
+    return 0;
+  }
+};
+
+export default async function Navbar() {
+  const cartItemsCount = await getCartItemsCount();
+
   return (
     <div>
-      <NavUI cartItemsCount={0} />
+      <NavUI cartItemsCount={cartItemsCount} />
     </div>
   );
 }
